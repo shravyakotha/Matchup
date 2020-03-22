5244e1fba2434d88ed62386abeae63722a924ccc
unknown <kothasairarun@gmail.com>
2020-03-22 09:35:30 +0530

    first commit

---
+++ match.js
@@ -0,0 +1,266 @@
+/*var famous = [
+    ["marina", "charminar", "camelsafari", "gatewayofindia", "onam"],
+    ["idlidosa", "biryani", "hawamahal", "pavbhaji", "jatayu"],
+    ["chennisuperkings", "sunrisershyd", "rajasthanroyals", "mumbaiindians", "kathakali"],
+    ["adiyogishivastatue", "golconda", "costume", "tajhotel", "houseboat"],
+    ["tamil", "telugu", "rajasthani", "marati", "malayalam"]
+]*/
+var famous = [
+    ["10","20","30" , "40" , "50"],
+    ["60" , "70" ,"80" ,"90" ,"100"],
+    ["110" , "120" ,"130" ,"140" ,"150"],
+    ["160" , "170" ,"180" ,"190" ,"200"],
+    ["210" , "220" ,"230" ,"240" ,"250"]
+]
+
+//var state = ["tamilnadu", "telengana", "rajasthan", "maharashtra"];
+
+// var tamilNadu = {
+//     food: "Idli_Dosa",
+//     beach: "Marina",
+//     place: "Adiyogi_shiva_statue",
+//     team: "Chenni_super_kings",
+//     language: "Tamil"
+// }
+
+
+// var telangana = {
+//     food: "Biryani",
+//     place1: "Charminar",
+//     place2: "Golconda",
+//     team: "Sunrisers_hyd",
+//     language: "Telugu"
+// }
+
+
+// var rajasthan = {
+//     lace1: "Camel_Safari",
+//     place2: "hawa_mahal",
+//     shopping: "Jewellary",
+//     place3: "Jantar_mantar",
+//     "language": "Rajasthani"
+// }
+
+// var maharashtra = {
+//     Cricket_team: "Mumbai Indians",
+//     place1: "Gateway_of_india",
+//     food: "av_bhaji",
+//     place2: "Marine_drive",
+//     language: "Marati"
+// }
+
+/*var place = window.prompt("Enter the place");
+var fame = window.prompt("Enter the famous thing");
+
+var i;
+for (i = 0; i < state.length; i++) {
+    if (place === state[i]) {
+        break;
+    }
+}
+
+console.log(i);
+console.log(checkFamous(i, fame));
+
+function checkFamous(i, fame) {
+    var k= 0;
+    for (let j = 0; j <famous.length; j++) {
+        
+        if (famous[j][i] == fame) {
+            k = 1;
+            return true;
+        }
+    }
+    if (k== 0) {
+        return false;
+    }
+}*/
+var place_value;
+
+function myFunctionState1() {
+    place_value = document.getElementById("myStateBtn1").value;
+    //console.log(place_value);
+    
+}
+
+
+function myFunctionState2() {
+    place_value = document.getElementById("myStateBtn2").value;
+    //console.log(place_value);
+    
+}
+
+function myFunctionState3() {
+    place_value = document.getElementById("myStateBtn3").value;
+  //  console.log(place_value);
+    
+}
+
+function myFunctionState4() {
+    place_value = document.getElementById("myStateBtn4").value;
+   // console.log(place_value);
+}
+
+function myFunctionState5() {
+    place_value = document.getElementById("myStateBtn5").value;
+   // console.log(place_value);
+}
+
+
+function myFunctionImg1() {
+    var fam = document.getElementById("myImgBtn1").value;
+   window.alert( checkFamous(place_value, fam));
+
+}
+
+function myFunctionImg2() {
+    var fam = document.getElementById("myImgBtn2").value;
+    window.alert(checkFamous(place_value, fam));
+}
+
+
+function myFunctionImg3() {
+    var fam = document.getElementById("myImgBtn3").value;
+    window.alert(checkFamous(place_value, fam));
+}
+
+function myFunctionImg4() {
+    var fam = document.getElementById("myImgBtn4").value;
+    window.alert(checkFamous(place_value, fam));
+}
+
+function myFunctionImg5() {
+    var fam = document.getElementById("myImgBtn5").value;
+    window.alert(checkFamous(place_value, fam));
+}
+
+
+function myFunctionImg6() {
+    var fam = document.getElementById("myImgBtn6").value;
+    console.log(checkFamous(place_value, fam));
+} 
+
+function myFunctionImg7() {
+    var fam = document.getElementById("myImgBtn7").value;
+    console.log(checkFamous(place_value, fam));
+}
+
+function myFunctionImg8() {
+    var fam = document.getElementById("myImgBtn8").value;
+    window.alert(checkFamous(place_value, fam));
+}
+
+function myFunctionImg9() {
+    var fam = document.getElementById("myImgBtn9").value;
+    window.alert(checkFamous(place_value, fam));
+}
+
+function myFunctionImg10() {
+    var fam = document.getElementById("myImgBtn10").value;
+    window.alert(checkFamous(place_value, fam));
+}
+
+function myFunctionImg11() {
+    var fam = document.getElementById("myImgBtn11").value;
+    window.alert(checkFamous(place_value, fam));
+}
+
+function myFunctionImg12() {
+    var fam = document.getElementById("myImgBtn12").value;
+    window.alert(checkFamous(place_value, fam));
+}
+
+function myFunctionImg13() {
+    var fam = document.getElementById("myImgBtn13").value;
+    window.alert(checkFamous(place_value, fam));
+}
+
+function myFunctionImg14() {
+    var fam = document.getElementById("myImgBtn14").value;
+    window.alert(checkFamous(place_value, fam));
+}
+
+function myFunctionImg15() {
+    var fam = document.getElementById("myImgBtn15").value;
+    window.alert(checkFamous(place_value, fam));
+}
+
+function myFunctionImg16() {
+    var fam = document.getElementById("myImgBtn16").value;
+    window.alert(checkFamous(place_value, fam));
+}
+
+function myFunctionImg17() {
+    var fam = document.getElementById("myImgBtn17").value;
+    window.alert(checkFamous(place_value, fam));
+}
+
+function myFunctionImg18() {
+    var fam = document.getElementById("myImgBtn18").value;
+    window.alert(checkFamous(place_value, fam));
+}
+
+function myFunctionImg19() {
+    var fam = document.getElementById("myImgBtn19").value;
+    window.alert(checkFamous(place_value, fam));
+}
+
+function myFunctionImg20() {
+    var fam = document.getElementById("myImgBtn20").value;
+    window.alert(checkFamous(place_value, fam));
+}
+
+function myFunctionImg21() {
+    var fam = document.getElementById("myImgBtn21").value;
+    window.alert(checkFamous(place_value, fam));
+}
+
+function myFunctionImg22() {
+    var fam = document.getElementById("myImgBtn22").value;
+    window.alert(checkFamous(place_value, fam));
+}
+
+function myFunctionImg23() {
+    var fam = document.getElementById("myImgBtn23").value;
+    window.alert(checkFamous(place_value, fam));
+}
+
+function myFunctionImg24() {
+    var fam = document.getElementById("myImgBtn24").value;
+    window.alert(checkFamous(place_value, fam));
+}
+
+function myFunctionImg25() {
+    var fam = document.getElementById("myImgBtn25").value;
+    window.alert(checkFamous(place_value, fam));
+}
+
+var k = 0;
+var score = 0;
+function checkFamous(place_value, fam) {
+    for (let i = 0; i < famous.length; i++) {
+
+        if (famous[i][place_value] === fam) {
+            k = 1;
+            // console.log("Place value is " + k);
+            // console.log(place_value);
+            score=score+1;
+            return "RIGHT" + "\nSCORE :" +score;
+            //return true;
+        
+        }
+    }
+   // return score;
+    return "WRONG";
+}
+
+
+
+
+
+
+
+
+
+// const timeoutId = setTimeout(callback, 60000);
+// clearTimeout(timeoutId);
